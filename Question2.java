32f5d7325bf84d31c97c400e1490e9e1d0ac3a0a
am021362 <qu950230@gmail.com>
2015-04-27 10:45:18 +0800

    B AM021362 蔡岳宏提交 editFile

--- Question2.java
+++ Question2.java
@@ -2,6 +2,19 @@
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
+private String readFile( String file ) throws IOException {
+    BufferedReader reader = new BufferedReader( new FileReader (file));
+    String         line = null;
+    StringBuilder  stringBuilder = new StringBuilder();
+    String         ls = System.getProperty("line.separator");
+
+    while( ( line = reader.readLine() ) != null ) {
+        stringBuilder.append( line );
+        stringBuilder.append( ls );
+    }
+
+    return stringBuilder.toString();
+}
 		imgCopy("Source.jpg", "Dest.jpg");
 	}
 }
